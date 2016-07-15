16bce7d425d140ef54121c49ec7cc7bafe25360c
Dion Jones <Dion.Jones@regus.com>
2016-07-15 14:04:01 +0100

    converted to a topshelf service https://topshelf.readthedocs.io/en/latest/

--- ProcessCourtesyCalls/Program.cs
+++ ProcessCourtesyCalls/Program.cs
@@ -1,15 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
+using System.Web.Services.Protocols;
+using Topshelf;
+using System.Configuration;
 
 namespace ProcessCourtesyCalls
 {
-    class Program
+    public class TownCrier
     {
-        static void Main(string[] args)
+        readonly Timer _timer;
+
+        public TownCrier()
         {
+            try
+            {
+                _timer = new Timer(Convert.ToDouble(ConfigurationManager.AppSettings["TimerPeriod"])) {AutoReset = true};
+            }
+            catch (OverflowException)
+            {
+            }
+            _timer.Elapsed += OnTimerOnElapsed;
+        }
+
+        private void OnTimerOnElapsed(object sender, ElapsedEventArgs eventArgs)
+        {
+            var obj = new local.regus.lombardi.CSTS_ExternalIntegrations();
+            Console.WriteLine(" Calling Web Methods");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("\n Calling processCourtesyCalls Method");
+            try
+            {
+                obj.processCourtesyCalls();
+            }
+            catch (SoapException error)
+            {
+                Console.WriteLine(error.Detail.ToString());
+            }
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+
+        }
+        public void Stop() { _timer.Stop(); }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(x =>                                 //1
+            {
+                x.Service<TownCrier>(s =>                        //2
+                {
+                    s.ConstructUsing(name => new TownCrier());     //3
+                    s.WhenStarted(tc => tc.Start());              //4
+                    s.WhenStopped(tc => tc.Stop());               //5
+                });
+                x.RunAsLocalSystem();                            //6
+
+                x.SetDescription("Courtesy Call Poller Host");        //7
+                x.SetDisplayName("CSTS CC Host");                       //8
+                x.SetServiceName("CCPollerHost");                       //9
+            });                                                  //10
         }
     }
 }
